a47e27f42c1058ed7a0aab30f99013cbbd751e27
VNKL <vnkl.iam@gmail.com>
2021-05-31 09:30:43 +0300

    change table head and tooltip

--- src/components/tables/campaigns-table-view/campaigns-table-view.js
+++ src/components/tables/campaigns-table-view/campaigns-table-view.js
@@ -36,7 +36,7 @@ const headCells = [
     { id: 'saves', align: 'right',  label: 'Добавл.', tooltip: 'Сохранения аудио и плейлистов из объявлений в аудиозаписях пользователей' },
     { id: 'cps', align: 'right',  label: 'CPS', tooltip: 'Cost Per Save - стоимость одного сохранения в рублях' },
     { id: 'str', align: 'right',  label: 'STR', tooltip: 'Save Through Rate - конверсия из показов в добавления' },
-    { id: 'cplCpsRate', align: 'right',  label: 'CPL/CPS', tooltip: 'Соотношение стоимости прослушивания к стоимости добавления' },
+    { id: 'cplCpsRate', align: 'right',  label: 'CPS/CPL', tooltip: 'Соотношение стоимостей добавления и прослушивания (сколько людей, нажавших на плей, добавили трек себе)' },
     { id: 'audienceCount', align: 'right',  label: 'Аудитория', tooltip: 'Сумма размеров аудиторий всех сегментов кампании' },
     { id: 'date', align: 'right',  label: 'Дата', tooltip: 'Дата создания кампании' },
 ]
